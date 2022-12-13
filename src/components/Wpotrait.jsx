eb31f329910ae18e19909e7514826b9dfefa8e0c
Brooklyn Kairupan <brokinignacio3@gmail.com>
2022-12-13 16:12:58 +0800

    newest

---
+++ src/components/Wpotrait.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Diving from '../assets/diving.jpg'
+
+const Wpotrait = () => {
+    return (
+        <div>
+            <img src={Diving} alt="" />
+        </div>
+    )
+}
+
+export default Wpotrait;
--- src/pages/detail1.jsx
+++ src/pages/detail1.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
-import WisataSatu from '../components/Wisata'
+import Wpotrait from '../components/Wpotrait'
+
 
 const Detail1 = () => {
     return (
-        <div>
-            <WisataSatu />
+        <div className='md:flex mx-8 space-x-3'>
             <div>
-                <p>
-
+                <Wpotrait />
+            </div>
+            <div>
+                <h1 className='font-bold'>Taman Nasional Bunaken</h1>
+                <p className=''>
+                    Taman Nasional Bunaken adalah taman laut yang terletak di Sulawesi Utara, Indonesia.
+                    Taman ini terletak di Segitiga Terumbu Karang yang menjadi habitat bagi 390 spesies terumbu karang
+                    dan juga berbagai spesies ikan, moluska, reptil, dan mamalia laut. Taman Nasional Bunaken merupakan
+                    perwakilan ekosistem laut br Indonesia, meliputi padang rumput laut, terumbu karang, dan ekosistem pantai.
                 </p>
             </div>
         </div>
     )
 }
 
-export default Detail1
+export default Detail1;
